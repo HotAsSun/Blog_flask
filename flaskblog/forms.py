f655a13e0cbb9a70a6f7c8f9f06dc19cf4329bec
HotAsSun <sina.khadem.885@gmail.com>
2025-09-30 12:27:36 +0330

    added login and logout and modified some files for easier use and modified the nav bar added the info to the database with ease and secure (hashing the pass mostly)

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired , Length , Email , EqualTo
+from wtforms.validators import DataRequired , Length , Email , EqualTo , ValidationError
+from flaskblog.models import User
+
 
 
 class RegistrationForm(FlaskForm):
@@ -14,11 +16,28 @@ class RegistrationForm(FlaskForm):
                              validators=[DataRequired(), Length(min=4,max=16),EqualTo('password')])
     submit = SubmitField('sign up')
 
+
+    def validate_username(self, username=None):
+        
+        username = User.query.filter_by(username = username.data).first()
+
+        if username:
+            raise ValidationError(f"the username :  {User.username} is already taken  ")
+        
+    def validate_email(self, email=None):
+        
+        email = User.query.filter_by(email = email.data).first()
+
+        if email:
+            raise ValidationError("the email is already taken please choose and other one  ")
+
+
+
 class LoginForm(FlaskForm):
     email = StringField('email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('password',
-                             validators=[DataRequired(), Length(min=8,max=63  ) ])
+                             validators=[DataRequired(), Length(min=4,max=63  ) ])
     remember = BooleanField('remember me ')
 
     submit = SubmitField('log in ')
